9ed2b4342359393974148b9bd7f18ec000a5d76c
Michael Defaveri <1michael.defaveri@gmail.com>
2026-02-19 17:31:37 -0300

    resolucao do exercicio de numero 11

--- Aula02/exercicios.py
+++ Aula02/exercicios.py
@@ -72,6 +72,11 @@ print(f'A área do círculo é de: {area}')
 # #### Strings (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+#%%
+string_user = str(input('Digite uma palavra: '))
+maius = string_user.upper()
+print(f'{string_user} convertido para maiúsculo é {maius}')
+
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
