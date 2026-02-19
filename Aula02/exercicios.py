9fa2aa9767a9bc917151bab3199cf7b5618d84f9
Michael Defaveri <1michael.defaveri@gmail.com>
2026-02-19 17:18:21 -0300

    resolucao do exercicio de numero 10

--- Aula02/exercicios.py
+++ Aula02/exercicios.py
@@ -59,6 +59,9 @@ print(f'O valor da potência de {base} por {expoente} é de: {resultado}')
 
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 # %%
+celsius = float(input('Digite a temperatura em Celsius: '))
+fahrenheit = (celsius * 9/5) + 32
+print(f'A temperatura de {celsius}°C é de {fahrenheit}°F')
 
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
 
