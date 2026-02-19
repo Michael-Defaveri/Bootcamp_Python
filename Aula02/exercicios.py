aa1b47188010c584ef9c950e08031ce2f87bebba
Michael Defaveri <1michael.defaveri@gmail.com>
2026-02-19 17:11:03 -0300

    resolucao de exercicios ate o de numero 7

---
+++ Aula02/exercicios.py
@@ -0,0 +1,82 @@
+# #### Inteiros (`int`)
+
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+# %%
+n1 = int(input("Digite um número inteiro: "))
+n2 = int(input("Digite outro número inteiro: "))
+resultado = n1 + n2
+print(f'O valor da soma de {n1} + {n2} é de:  {resultado}')
+
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+# %%
+n_user = int(input('Digite um número: '))
+resto_div = n_user % 5
+print(f'O resto da divisão de {n_user} por 5 é de: {resto_div}')
+
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+# %%
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+resultado = n1 * n2
+print(f'O valor da multiplicação de {n1} por {n2} é de: {resultado}')
+
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+#%%
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+resultado = n1 // n2
+print(f'O valor da divisão inteiro de {n1} por {n2} é de: {resultado}')
+
+# 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+# %%
+number_user = int(input('Digite um número: '))
+resultado = number_user ** 2
+print(f'O valor do quadrado de {number_user} é de: {resultado}')
+
+
+# #### Números de Ponto Flutuante (`float`)
+
+# 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
+#%%
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite outro número: '))
+resultado = n1 = n2
+print(f'O valor da soma dos dois número é de: {resultado}')
+
+# 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+# %%
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite outro número: '))
+resultado = (n1 + n2) / 2
+print(f'O valor da média de {n1} e {n2} é de: {resultado}')
+
+# 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+#%%
+
+
+# 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+# 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+
+# #### Strings (`str`)
+
+# 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+# 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+# 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+# 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+# 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+# #### Booleanos (`bool`)
+
+# 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+# 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+# 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+# 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+# 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+
+# #### try-except e if
+
+# 21: Conversor de Temperatura
+# 22: Verificador de Palíndromo
+# 23: Calculadora Simples
+# 24: Classificador de Números
+# 25: Conversão de Tipo com Validação
