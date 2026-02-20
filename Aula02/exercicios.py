07170cc7bb97509319c4555826899a00714177e1
Michael Defaveri <1michael.defaveri@gmail.com>
2026-02-20 19:39:02 -0300

    resolucao do exercicio de numero 25

--- Aula02/exercicios.py
+++ Aula02/exercicios.py
@@ -235,3 +235,12 @@ except ValueError:
         
 # 25: Conversão de Tipo com Validação
 # %%
+entrada_lista = input("Digite uma lista de números separados por vírgula: ")
+numeros_str = entrada_lista.split(",")
+numeros_int = []
+try:
+    for num in numeros_str:
+        numeros_int.append(int(num.strip()))
+    print("Lista de inteiros:", numeros_int)
+except ValueError:
+    print("Erro: certifique-se de que todos os elementos são números inteiros válidos.")
