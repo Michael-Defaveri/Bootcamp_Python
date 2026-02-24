64d1bbdecd23445110b4e60166c4290b0ab26c8a
Michael Defaveri <1michael.defaveri@gmail.com>
2026-02-23 22:12:48 -0300

    resolucao do exercicios de numero 5

--- Aula03/exercicios.py
+++ Aula03/exercicios.py
@@ -120,4 +120,15 @@ elif idade_user3 >= 18 and idade_user3 <= 65 and email_user3 == email_user3:
 
 else:
     print('Dados de usuário inválidos')
+
+
+# 5. Detecção de Anomalias em Dados de Transações
+# Você está trabalhando em um sistema de detecção de fraude e precisa identificar transações suspeitas. 
+# Uma transação é considerada suspeita se o valor for superior a R$ 10.000 ou se ocorrer fora do horário comercial (antes das 9h ou depois das 18h). 
+# Dada uma transação como transacao = {'valor': 12000, 'hora': 20}, verifique se ela é suspeita.
+
 # %%
+transacao = {'valor': 12000, 'hora': 20}
+
+if transacao['valor'] > 10000 or transacao['hora'] < 9 or transacao['hora'] > 18:
+    print('Transação suspeita')
