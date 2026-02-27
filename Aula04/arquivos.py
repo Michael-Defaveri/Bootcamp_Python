e72171c646ba7352a90c49fc609a0820126f1f42
Michael Defaveri <1michael.defaveri@gmail.com>
2026-02-27 15:33:21 -0300

    lendo arquivo csv no modo raiz

---
+++ Aula04/arquivos.py
@@ -0,0 +1,14 @@
+import csv
+
+caminho_arquivo: str = 'Aula04/exemplo.csv'
+arquivo_csv: list = []
+
+# abrindo uma janela de contexto com with
+with open(file=caminho_arquivo, mode='r', encoding='utf-8') as arquivo:
+    leitor_csv = csv.DictReader(arquivo)
+
+    for i in leitor_csv:
+        arquivo_csv.append(i)
+
+print(arquivo_csv)
+
