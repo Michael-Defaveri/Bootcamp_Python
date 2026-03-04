5bae24c9e9275b295b7b05e967792daa0a76776c
Michael Defaveri <1michael.defaveri@gmail.com>
2026-03-04 15:29:05 -0300

    resolucao do exercicios avancado 6

---
+++ Aula04/exercicios_intermediario_avancado.py
@@ -0,0 +1,69 @@
+# 6.Eliminação de Duplicatas
+# Objetivo: Dada uma lista de emails, remover todos os duplicados.
+
+email: list = ['michael@gmail.com', 'michael@gmail.com', 'sandra@gmail.com', 'isadora@gmail.com']
+email_unicos = set(email)
+
+print(email_unicos)
+
+
+
+# 7. Filtragem de Dados
+# Objetivo: Dada uma lista de idades, filtrar apenas aquelas que são maiores ou iguais a 18.
+
+
+
+
+
+# 8. Ordenação Personalizada
+# Objetivo: Dada uma lista de dicionários representando pessoas, ordená-las pelo nome.
+
+
+
+
+# 9. Agregação de Dados
+# Objetivo: Dado um conjunto de números, calcular a média.
+
+
+
+
+# 10. Divisão de Dados em Grupos
+# Objetivo: Dada uma lista de valores, dividir em duas listas: uma para valores pares e outra para ímpares.
+
+
+
+
+
+# Exercícios com Dicionários
+# 11. Atualização de Dados
+# Objetivo: Dada uma lista de dicionários representando produtos, atualizar o preço de um produto específico.
+
+
+
+
+
+# 12. Fusão de Dicionários
+# Objetivo: Dados dois dicionários, fundi-los em um único dicionário.
+
+
+
+
+
+# 13. Filtragem de Dados em Dicionário
+# Objetivo: Dado um dicionário de estoque de produtos, filtrar aqueles com quantidade maior que 0.
+
+
+
+
+
+
+# 14. Extração de Chaves e Valores
+# Objetivo: Dado um dicionário, criar listas separadas para suas chaves e valores.
+
+
+
+
+
+
+# 15. Contagem de Frequência de Itens
+# Objetivo: Dada uma string, contar a frequência de cada caractere usando um dicionário.
